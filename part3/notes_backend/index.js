11161149deec6524d8a1ba1342f8e1cb3274ec62
Aadi Nesarkar <aadi.nesarkar@gmail.com>
2022-09-25 13:24:38 +0530

    Connected notes backend to MongoDB

--- part3/notes_backend/index.js
+++ part3/notes_backend/index.js
@@ -1,10 +1,25 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const Note = require('./models/note');
+const { query } = require('express');
 
 app.use(express.json());
 app.use(cors());
 app.use(express.static('build'));
+
+const errorHandler = (error, request, response, next) => {
+	console.error(error.message);
+
+	if (error.name === 'CastError') {
+		return response.status(400).send({ error: 'malformed id' });
+	} else if (error.name === 'ValidationError') {
+		return response.status(400).json({ error: error.message });
+	}
+	next(error);
+};
+
+app.use(errorHandler);
 
 let notes = [
 	{
@@ -32,33 +47,32 @@ app.get('/', (request, response) => {
 });
 
 app.get('/api/notes', (request, response) => {
-	response.json(notes);
+	Note.find({}).then((notes) => {
+		response.json(notes);
+	});
 });
 
-app.get('/api/notes/:id', (request, response) => {
-	const id = Number(request.params.id);
-	const note = notes.find((note) => note.id === id);
-
-	if (note) {
-		response.json(note);
-	} else {
-		response.status(404).end();
-	}
+app.get('/api/notes/:id', (request, response, next) => {
+	Note.findById(request.params.id)
+		.then((note) => {
+			if (note) {
+				response.json(note);
+			} else {
+				response.status(404).end();
+			}
+		})
+		.catch((error) => next(error));
 });
 
 app.delete('/api/notes/:id', (request, response) => {
-	const id = Number(request.params.id);
-	notes = notes.filter((note) => note.id !== id);
-
-	response.status(294).end();
+	Note.findByIdAndRemove(request.params.id)
+		.then((result) => {
+			response.status(204).end();
+		})
+		.catch((error) => next(error));
 });
 
-const generateId = () => {
-	const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
-	return maxId + 1;
-};
-
-app.post('/api/notes', (request, response) => {
+app.post('/api/notes', (request, response, next) => {
 	const body = request.body;
 
 	if (!body.content) {
@@ -67,26 +81,39 @@ app.post('/api/notes', (request, response) => {
 		});
 	}
 
-	const note = {
+	const note = new Note({
 		content: body.content,
 		important: body.important || false,
 		date: new Date(),
-		id: generateId(),
-	};
-	notes = notes.concat(note);
-	response.json(note);
+	});
+	note
+		.save()
+		.then((savedNote) => {
+			response.json(savedNote);
+		})
+		.catch((error) => next(error));
 });
 
 app.put('/api/notes/:id', (request, response) => {
+	const { content, important } = request.body;
+
 	const note = {
-		...request.body,
-		important: request.body.important,
+		content: body.content,
+		important: body.important,
 	};
-	notes = notes.map((n) => (n.id !== note.id ? n : note));
-	response.json(note);
+
+	Note.findByIdAndUpdate(
+		request.params.id,
+		{ content, important },
+		{ new: true, runValidators: true, context: query }
+	)
+		.then((updatedNote) => {
+			response.json(updatedNote);
+		})
+		.catch((error) => next(error));
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`);
 });
